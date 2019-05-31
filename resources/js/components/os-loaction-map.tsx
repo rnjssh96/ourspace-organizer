ea1fa84f9470f6257e281c99e642fcc4b0611a8f
Sookhyun Kwon <rnjssh96@gmail.com>
2019-06-01 00:56:50 +0900

    Install google-map

--- resources/js/components/os-loaction-map.tsx
+++ resources/js/components/os-loaction-map.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
+import { compose, withProps } from 'recompose';
+import {
+    GoogleMap,
+    withScriptjs,
+    withGoogleMap,
+    Marker,
+} from 'react-google-maps';
 
-export default class OSLoactionMap extends React.Component {
-    // private _renderMap() {
-    //     map = new google.maps.Map(document.getElementById('map'), {
-    //         center: { lat: -34.397, lng: 150.644 },
-    //         zoom: 8,
-    //     });
-    // }
+const OSLoactionMap = compose(
+    withProps({
+        googleMapURL:
+            'https://maps.googleapis.com/maps/api/js?key=AIzaSyDQKkNsepBOaIiSSp4OUIFZGKmCOFTrho4&callback=initMap',
+        loadingElement: <div style={{ height: '100%' }} />,
+        containerElement: <div id="os-location-map" />,
+        mapElement: <div id="map" />,
+    }),
+    withScriptjs,
+    withGoogleMap,
+)(props => (
+    <GoogleMap
+        defaultZoom={8}
+        defaultCenter={{ lat: -34.397, lng: 150.644 }}
+        mapTypeId={'roadmap'}
+    >
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    </GoogleMap>
+));
 
-    render() {
-        return (
-            <div id="os-location-map">
-                {/* <div id="map" />
-                <script
-                    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDQKkNsepBOaIiSSp4OUIFZGKmCOFTrho4&callback=initMap"
-                    async
-                    defer
-                /> */}
-            </div>
-        );
-    }
-}
+export default OSLoactionMap;
