a72c9d3cc20c2c38f6fe0a5f96a6f2efe83a7cc5
Sookhyun Kwon <rnjssh96@gmail.com>
2019-08-27 19:11:11 +0900

    Rename package name

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # ourspace-organizer
+
 OurSpace Organizer Web-app
 
 read https://www.howtoforge.com/tutorial/install-laravel-on-ubuntu-for-apache/ for server-setup
-read https://devmarketer.io/learn/setup-laravel-project-cloned-github-com/ for project-setup
+
+1. Project Setup
+
+    1. Clone project:
+       '''git clone https://github.com/rnjssh96/ourspace-organizer.git'''
+
+    2. Install Composer Dependencies
+       '''composer install'''
+
+    3. Install NPM Dependencies
+       '''npm install'''
+
+    4. Setup '''.env''' file
+       '''cp .env.example .env'''
+
+    5. Generate an app encryption key
+       '''php artisan key:generate'''
--- public/js/app.js
+++ public/js/app.js
@@ -79401,8 +79401,8 @@ exports.searchSpaceList = (spaceList, query) => async (dispatch) => {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/Sookhyun/Desktop/our-space-organizer/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/Sookhyun/Desktop/our-space-organizer/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/Sookhyun/Desktop/ourspace-organizer/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/Sookhyun/Desktop/ourspace-organizer/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
